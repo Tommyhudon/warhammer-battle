04b65b40a8c17a911191a8ec849bc7d47392a222
Simon Champetier <simon.champetier@outlook.com>
2021-10-11 17:29:37 -0400

    Can now add temporary effects on unit stats

--- src/main/java/com/whbattle/springboot/usecase/battlereport/CreateBattleReport.java
+++ src/main/java/com/whbattle/springboot/usecase/battlereport/CreateBattleReport.java
@@ -8,7 +8,10 @@ import com.whbattle.springboot.usecase.unit.dto.UnitMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 @Component
 public class CreateBattleReport {
@@ -16,47 +19,58 @@ public class CreateBattleReport {
     private final UnitMapper unitMapper;
 
     @Autowired
-    public CreateBattleReport(UnitMapper unitMapper){
+    public CreateBattleReport(UnitMapper unitMapper) {
         this.unitMapper = unitMapper;
     }
 
     public BattleReport create(UnitDto[] unitDtos) {
         int nbOfWin = 0;
+        int nbOfSurvivors = 0;
         int NUMBER_OF_BATTLE = 10000;
         Unit[] units = unitMapper.mapUnitList(unitDtos);
 
-
         for (int i = 0; i < NUMBER_OF_BATTLE; i++) {
-            List<Unit> shuffledList = this.cloneUnits(units);
-            Collections.shuffle(shuffledList, new Random());
+            List<Unit> shuffledUnits = this.cloneUnits(unitMapper.mapUnitList(unitDtos));
+            Collections.shuffle(shuffledUnits, new Random());
 
-            Unit winner = this.battle(shuffledList);
+            Unit winner = this.battle(shuffledUnits);
             if (winner.getName().equals(units[0].getName())) {
                 nbOfWin++;
+                nbOfSurvivors += winner.getNumber();
             }
         }
 
-        float probability = (float) nbOfWin/NUMBER_OF_BATTLE;
-        return new BattleReport(probability, units[0], units[1]);
+        float probability = (float) nbOfWin / NUMBER_OF_BATTLE;
+        float averageSurvivingModels = (float) nbOfSurvivors / NUMBER_OF_BATTLE;
+        return new BattleReport(probability, averageSurvivingModels, units[0], units[1]);
     }
 
     public Unit battle(List<Unit> units) {
         Unit winner = null;
+        int currentTurn = 0;
 
-        while(units.size() >= 2) {
-            Attack attack = units.get(0).rollAttacks();
-            units.get(1).resolveDamage(attack);
+        Unit unit1 = units.get(0);
+        Unit unit2 = units.get(1);
 
-            if (units.get(1).isDead()) {
-                winner = units.get(0);
+        while (units.size() >= 2) {
+            currentTurn++;
+
+            unit1.updateEffects(currentTurn);
+            unit2.updateEffects(currentTurn);
+
+            Attack attack = unit1.rollAttacks();
+            unit2.resolveDamage(attack);
+
+            if (unit2.isDead()) {
+                winner = unit1;
                 break;
             }
 
-            attack = units.get(1).rollAttacks();
-            units.get(0).resolveDamage(attack);
+            attack = unit2.rollAttacks();
+            unit1.resolveDamage(attack);
 
-            if (units.get(0).isDead()) {
-                winner = units.get(1);
+            if (unit1.isDead()) {
+                winner = unit2;
                 break;
             }
         }
@@ -67,7 +81,7 @@ public class CreateBattleReport {
     private List<Unit> cloneUnits(Unit[] units) {
         List<Unit> resultList = new ArrayList<>();
 
-        for(Unit unit: units) {
+        for (Unit unit : units) {
             resultList.add(unit.clone());
         }
 
--- src/main/java/com/whbattle/springboot/usecase/unit/dto/UnitMapper.java
+++ src/main/java/com/whbattle/springboot/usecase/unit/dto/UnitMapper.java
@@ -2,12 +2,17 @@ package com.whbattle.springboot.usecase.unit.dto;
 
 import com.whbattle.springboot.domain.entity.dice.DiceRoller;
 import com.whbattle.springboot.domain.entity.dice.Die;
+import com.whbattle.springboot.domain.entity.unit.Effect;
 import com.whbattle.springboot.domain.entity.unit.Unit;
 import com.whbattle.springboot.domain.entity.unit.weapon.Weapon;
+import com.whbattle.springboot.usecase.effect.dto.dto.EffectDto;
+import com.whbattle.springboot.usecase.effect.dto.dto.EffectMapper;
 import com.whbattle.springboot.usecase.weapon.dto.WeaponDto;
 import com.whbattle.springboot.usecase.weapon.dto.WeaponMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class UnitMapper {
@@ -15,9 +20,14 @@ public class UnitMapper {
     @Autowired
     private final WeaponMapper weaponMapper;
 
-    public UnitMapper(WeaponMapper weaponMapper) {
+    @Autowired
+    private final EffectMapper effectMapper;
+
+    public UnitMapper(WeaponMapper weaponMapper, EffectMapper effectMapper) {
         this.weaponMapper = weaponMapper;
+        this.effectMapper = effectMapper;
     }
+
     public Unit mapUnit(UnitDto unitDto) {
         return new Unit(
                 new DiceRoller(new Die()),
@@ -27,15 +37,20 @@ public class UnitMapper {
                 unitDto.wound,
                 unitDto.totalWounds,
                 mapWeapon(unitDto.weapon),
-                unitDto.reRoll);
+                unitDto.reRoll,
+                mapEffectList(unitDto.effects));
     }
 
     public Unit[] mapUnitList(UnitDto[] unitDtos) {
         Unit[] units = new Unit[unitDtos.length];
-        for(int i = 0;  i < unitDtos.length; i++) {
+        for (int i = 0; i < unitDtos.length; i++) {
             units[i] = this.mapUnit(unitDtos[i]);
         }
         return units;
+    }
+
+    public List<Effect> mapEffectList(EffectDto[] effectDtos) {
+        return effectMapper.mapEffectList(effectDtos);
     }
 
     private Weapon mapWeapon(WeaponDto weaponDto) {
